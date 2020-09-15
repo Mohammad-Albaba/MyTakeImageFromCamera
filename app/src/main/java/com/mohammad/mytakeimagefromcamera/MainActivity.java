a98c0771132012947e540fc375be76fbb3b3bb55
Mohammad Albaba <albabam9@gmail.com>
2020-09-16 01:02:07 +0300

    Initial commit

---
+++ app/src/main/java/com/mohammad/mytakeimagefromcamera/MainActivity.java
@@ -0,0 +1,38 @@
+package com.mohammad.mytakeimagefromcamera;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+ImageView imageview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageview=findViewById(R.id.imageview);
+    }
+
+    public void btn_Take_photo(View view) {
+        Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==100 && resultCode==RESULT_OK){
+            imageview.setImageBitmap((Bitmap)data.getExtras().get("data"));
+
+
+        }
+    }
+}
+
